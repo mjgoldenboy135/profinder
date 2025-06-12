f1d22a7d419f8ab5d660552a58439d29ad8483bb
Mohsin Jamal <mohsinjamal135@gmail.com>
2025-06-12 16:47:30 +0000

    This was the error while uploading file

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,10 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    allowedDevOrigins: ['http://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev'],
+    allowedDevOrigins: [
+        'http://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
+        'https://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev'
+    ],
   }
 };
 
