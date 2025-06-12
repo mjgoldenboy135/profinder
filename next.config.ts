fba27e55c22e565b884a8182dedca8eeb1ea48a3
Mohsin Jamal <mohsinjamal135@gmail.com>
2025-06-12 23:41:26 +0000

    why iam not able to upload profile pic

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,8 @@ const nextConfig: NextConfig = {
   experimental: {
     allowedDevOrigins: [
         'http://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
-        'https://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev'
+        'https://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
+        'http://localhost:9002' // Added localhost for dev server
     ],
   }
 };
