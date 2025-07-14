5fbe0038dc2c9c2bba4ca14fe5ac2feda485bc6c
Mohsin Jamal <mjgoldenboy135@gmail.com>
2025-07-14 04:06:08 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   env: {
     // This maps the server-side variable (from apphosting.yaml) 
     // to the name the client-side code expects.
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.google_maps_api_key,
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -46,3 +46,51 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import type {NextConfig} from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   env: {
+//     // This maps the server-side variable (from apphosting.yaml) 
+//     // to the name the client-side code expects.
+//     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'placehold.co',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'firebasestorage.googleapis.com',
+//         port: '',
+//         pathname: '/v0/b/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'lh3.googleusercontent.com', // Added for Google User profile images
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+//   experimental: {
+//     allowedDevOrigins: [
+//         'http://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
+//         'https://9000-firebase-studio-1749131851806.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
+//         'http://localhost:9002' // Added localhost for dev server
+//     ],
+//   }
+// };
+
+// export default nextConfig;
